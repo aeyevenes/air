c3d651b3fa1002e8296f2d1a990e57fe2274b1dd
alejandro yevenes <aeyevenes@gmail.com>
2021-08-25 09:38:13 -0400

    rails new

---
+++ db/migrate/20210825003135_create_flats.rb
@@ -0,0 +1,12 @@
+class CreateFlats < ActiveRecord::Migration[6.1]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.integer :number_of_guests
+
+      t.timestamps
+    end
+  end
+end
